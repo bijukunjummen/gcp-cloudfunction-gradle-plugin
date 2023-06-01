998e51576fcf0efe23be756434d3cdd22cd26c03
Biju Kunjummen <biju.kunjummen@gmail.com>
2023-06-01 10:08:58 -0700

    Sets classpath different from input files for task

--- src/main/java/com/github/bijukunjummen/cloudfunction/task/CloudFunctionRunTask.java
+++ src/main/java/com/github/bijukunjummen/cloudfunction/task/CloudFunctionRunTask.java
@@ -2,8 +2,12 @@ package com.github.bijukunjummen.cloudfunction.task;
 
 import com.github.bijukunjummen.cloudfunction.CloudFunctionRunExtension;
 import com.github.bijukunjummen.cloudfunction.Constants;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.SourceSet;
@@ -11,44 +15,54 @@ import org.gradle.api.tasks.SourceSet;
 import java.util.List;
 
 /**
- * Task responsible for starting the Google Cloud function invoker, a local instance of the running function
- * which can be integration tested against
+ * Task responsible for starting the Google Cloud function invoker, a local instance of the running
+ * function which can be integration tested against
  *
  * @author Biju Kunjummen
  */
 public class CloudFunctionRunTask extends JavaExec {
-    public static final String INVOKER_MAIN_CLASS = "com.google.cloud.functions.invoker.runner.Invoker";
-    public static final String RUNTIME_CLASSPATH = "runtimeClasspath";
-    public static final String MAIN_SOURCESET = "main";
-    private CloudFunctionRunExtension invokerExtension;
 
-    public CloudFunctionRunTask() {
-        getMainClass().set(INVOKER_MAIN_CLASS);
-        JavaPluginExtension javaPluginExtension = getProject().getExtensions().getByType(JavaPluginExtension.class);
-        ConfigurationContainer configurations = getProject().getConfigurations();
-        Configuration invokerConfiguration = configurations.getAt(Constants.INVOKER_CONFIGURATION_NAME);
-        Configuration runtimeClasspathConfiguration = configurations.getAt(RUNTIME_CLASSPATH);
-        SourceSet main = javaPluginExtension.getSourceSets().getByName(MAIN_SOURCESET);
-        this.setClasspath(invokerConfiguration);
-        this.getInputs().files(runtimeClasspathConfiguration, main.getOutput());
+  public static final String INVOKER_MAIN_CLASS = "com.google.cloud.functions.invoker.runner.Invoker";
+  public static final String RUNTIME_CLASSPATH = "runtimeClasspath";
+  public static final String MAIN_SOURCESET = "main";
+  private List<File> classpathFileLocations = new ArrayList<>();
+  private CloudFunctionRunExtension invokerExtension;
+
+  public CloudFunctionRunTask() {
+    getMainClass().set(INVOKER_MAIN_CLASS);
+    JavaPluginExtension javaPluginExtension = getProject().getExtensions()
+        .getByType(JavaPluginExtension.class);
+    ConfigurationContainer configurations = getProject().getConfigurations();
+    Configuration invokerConfiguration = configurations.getAt(Constants.INVOKER_CONFIGURATION_NAME);
+    Configuration runtimeClasspathConfiguration = configurations.getAt(RUNTIME_CLASSPATH);
+
+    SourceSet main = javaPluginExtension.getSourceSets().getByName(MAIN_SOURCESET);
+    this.setClasspath(invokerConfiguration);
+    this.getInputs().files(runtimeClasspathConfiguration, main.getOutput());
+    this.classpathFileLocations.addAll(runtimeClasspathConfiguration.getFiles());
+    this.classpathFileLocations.addAll(main.getOutput().getFiles());
+  }
+
+  public void setInvokerExtension(CloudFunctionRunExtension invokerExtension) {
+    this.invokerExtension = invokerExtension;
+  }
+
+  @Override
+  public void exec() {
+    String classpathFileCollection = this.classpathFileLocations
+        .stream()
+        .map(file -> file.toString())
+        .collect(Collectors.joining(File.pathSeparator));
+
+    this.setArgs(
+        List.of(
+            "--target", invokerExtension.getTarget().get(),
+            "--port", invokerExtension.getPort().get(),
+            "--classpath", classpathFileCollection));
+
+    if (!invokerExtension.getTestMode().get()) {
+      super.exec();
     }
 
-    public void setInvokerExtension(CloudFunctionRunExtension invokerExtension) {
-        this.invokerExtension = invokerExtension;
-    }
-
-    @Override
-    public void exec() {
-        String classpathFileCollection = this.getInputs().getFiles().getAsPath();
-        this.setArgs(
-                List.of(
-                        "--target", invokerExtension.getTarget().get(),
-                        "--port", invokerExtension.getPort().get(),
-                        "--classpath", classpathFileCollection));
-
-        if (!invokerExtension.getTestMode().get()) {
-            super.exec();
-        }
-
-    }
+  }
 }
